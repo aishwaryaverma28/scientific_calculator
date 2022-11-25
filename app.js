e7222ef88b98a56e6423a5b93a184354a667e13e
Aishwarya <aishwarya.verma28@gmail.com>
2022-11-25 17:14:29 +0530

    design complete

---
+++ app.js
@@ -0,0 +1,12 @@
+let screen=document.querySelector("#screen");
+let btn=document.querySelectorAll(".btn");
+btn.forEach(item=>{
+    // item.addEventListener("click",(e)=>
+    // btntext=e.target.innerText;
+    // if(btnText=='*')
+    // btnText="*";
+
+    // screen.value+=btnText
+    // )
+    console.log(item);
+})
